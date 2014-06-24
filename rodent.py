7d8d81b4dcd68a6f53196aa1a802041882c2de57
Dmitry Kireev <dmitry@kireev.co>
2014-06-23 17:17:49 -0700

    add confg.py

--- rodent.py
+++ rodent.py
@@ -4,13 +4,28 @@
 from __future__ import unicode_literals
 __author__ = 'dmitry'
 
-
+import config
 import struct
 import re
+import sys
 import os
+import subprocess as sb
 import dns.name
 from flask import Flask
+import logging
 from flask import jsonify,make_response, request, abort
+import socket
+
+## Logging
+logging.basicConfig(format='%(asctime)s %(message)s', filename='rodent.log', level=logging.DEBUG, stream=sys.stdout)
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
 
 
 app = Flask(__name__)
@@ -19,6 +34,7 @@ app = Flask(__name__)
 ## global (replace by configuration params)
 # server = 'ns1.docstoc.corp'
 
+################ DNS ################
 @app.route('/dns/', methods=['POST'])
 def create_dns():
     # fqdn=test15.docstoc.corp&value=192.168.5.1&type=A
@@ -28,39 +44,45 @@ def create_dns():
         value = request.args.get('value')
         type = request.args.get('type')
 
-
-
-
         if not fqdn:
             return make_response(jsonify({'error': 'fqdn must be present'}), 404)
         if not value:
             return make_response(jsonify({'error': 'value must be present'}), 404)
         if not type:
             return make_response(jsonify({'error': 'type must be present'}), 404)
+
         zone = dns.name.from_text(fqdn).split(3)[1].to_text(omit_final_dot=True)
 
-        if is_64_bit():
-            cmd = r'c:\windows\sysnative\cmd.exe /c '
-        else:
-            cmd = r'c:\windows\system32\cmd.exe /c '
+        if not dns_found(fqdn):
+            if is_64_bit():
+                cmd = r'c:\windows\sysnative\cmd.exe /c '
+            else:
+                cmd = r'c:\windows\system32\cmd.exe /c '
 
-        command = cmd + "c:\Windows\System32\dnscmd.exe /RecordAdd #{zone} #{fqdn}. #{type} #{value}"
+            command = cmd + "c:\Windows\System32\dnscmd.exe /RecordAdd #{zone} #{fqdn}. #{type} #{value}"
+            print command
 
-        os.call(command)
+            sb.call(command)
 
-        message = 'OK ' + zone, "cmd:", command
+            message = 'OK ' + zone, "cmd:", command
 
-        return make_response(jsonify({'message': message}), 200)
+            return make_response(jsonify({'message': message}), 200)
     else:
         message = 'Not implemented for ' + os.name
         return make_response(jsonify({'error': message}), 500)
 
 
+def dns_found(hostname):
+    try:
+        socket.gethostbyname(hostname)
+        return True
+    except socket.error:
+        return False
+################################################
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
-
+    return make_response(jsonify({'error': 'Not found' }), 404)
 
 
 
@@ -70,5 +92,7 @@ def is_64_bit():
     if (8 * struct.calcsize("P")) == 64:
         return True
 
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host=config.BIND_IP)
